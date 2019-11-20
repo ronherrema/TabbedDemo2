c0b07e2ea29373de9035b9e156e86be229cece7f
Ron Herrema <rherrema@gmail.com>
2019-11-20 21:14:57 +0000

    Added comments.

--- TabbedDemo2/Data.swift
+++ TabbedDemo2/Data.swift
@@ -1,11 +1,12 @@
 
 import Foundation
 
+// This class exists in order to provide shared data via the static property
  class Data {
     // type properties use the 'static' keyword
     static var data = [34, 45, 56, 67, 78]
     
-    // preventing the creation of Data objects with 'private'
+    // using 'private' to prevent the creation of Data objects 
     private init() {
     }
     
